43248cd86bfda87c8311240b930f3b8a733eb79d
SkPipul <skjabed252@gmail.com>
2022-10-25 20:23:33 +0600

    navbar / header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,34 @@
-// import React from 'react';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import SideBar from '../SideBar/SideBar';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import SideBar from '../SideBar/SideBar';
 
-// const Header = () => {
-//     return (
-//         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//       <Container>
-//         <Navbar.Brand href="#home" className='fw-bold'>Learning with SkPipul</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//         <Navbar.Collapse id="responsive-navbar-nav">
-//           <Nav className="mx-auto">
-//             <Nav.Link href="#features">Home</Nav.Link>
-//             <Nav.Link href="#pricing">Courses</Nav.Link>
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home" className='fw-bold'>Learning with SkPipul</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Courses</Nav.Link>
             
-//           </Nav>
-//           <Nav>
-//             <Nav.Link href="#deets">Login</Nav.Link>
-//             <Nav.Link eventKey={2} href="#memes">
-//               Sign Up
-//             </Nav.Link>
-//           </Nav>
-//           <div className='d-lg-none'>
-//             <SideBar></SideBar>
-//           </div>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//     );
-// };
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+          <div className='d-lg-none'>
+            <SideBar></SideBar>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+};
 
-// export default Header;
+export default Header;
