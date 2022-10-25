bbf4e2bc251a7a89e54fc2044dd2dbb63e2cb099
SkPipul <skjabed252@gmail.com>
2022-10-25 20:20:48 +0600

    Main section added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,25 +1,25 @@
-// import React from 'react';
-// import { Col, Container, Row } from 'react-bootstrap';
-// import { Outlet } from 'react-router-dom';
-// import Header from '../components/Header/Header';
-// import SideBar from '../components/SideBar/SideBar';
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import SideBar from '../components/SideBar/SideBar';
 
-// const Main = () => {
-//     return (
-//         <div>
-//             <Header></Header>
-//             <Container>
-//                 <Row>
-//                     <Col lg="4" className='d-none d-lg-block'>
-//                         <SideBar />
-//                     </Col>
-//                     <Col lg="8">
-//                         <Outlet></Outlet>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//         </div>
-//     );
-// };
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg="4" className='d-none d-lg-block'>
+                        <SideBar />
+                    </Col>
+                    <Col lg="8">
+                        <Outlet></Outlet>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
-// export default Main;
+export default Main;
